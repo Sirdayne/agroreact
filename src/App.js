b35e63c7135931db2637d72bd886d594fe238611
Sirdayne <mussinyernar@gmail.com>
2018-06-10 17:13:01 +0600

    added redux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
 import React, { Component } from 'react'
-import { createStore } from 'redux'
-import { connect } from 'react-redux'
-
 import logo from './logo.svg'
 import './App.css'
-
-const initialState = {
-  name: 'Ernar',
-  surname: 'Mussin'
-}
-
-const rootReducer = (state = initialState, action) => {
-  if (action.type === 'CHANGE_NAME') {
-    state.name = action.payload
-  }
-  if (action.type === 'CHANGE_SURNAME') {
-    state.surname = action.payload
-  }
-  return state
-}
-
-const store = createStore(rootReducer)
-
-console.log(store.getState())
 
 class App extends Component {
   render() {
@@ -35,8 +13,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Surname" />  
+        <input type="text" placeholder="Name" value={this.props.name} />
+        <input type="text" placeholder="Surname" value={this.props.surname}/>  
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,49 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux'
+import { connect, Provider } from 'react-redux'
+
+const initialState = {
+    name: 'Ernar',
+    surname: 'Mussin'
+}
+
+const actionChangeName = {
+    type: 'CHANGE_NAME',
+    payload: null
+}
+
+const actionChangeSurname = {
+    type: 'CHANGE_SURNAME',
+    payload: null
+}
+
+const rootReducer = (state = initialState, action) => {
+    if (action.type === 'CHANGE_NAME') {
+        state.name = action.payload
+    }
+    if (action.type === 'CHANGE_SURNAME') {
+        state.surname = action.payload
+    }
+return state
+}
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+console.log(store.getState())
+
+const mapStateToProps = (state) => {
+    console.log(state)
+    return {
+        name: state.name,
+        surname: state.surname
+    }
+}
+
+const WrapComponent = connect(mapStateToProps)(App)
+
+ReactDOM.render(<Provider store={store}>
+        <WrapComponent />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
